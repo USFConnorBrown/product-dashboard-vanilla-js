5833a6cb528a6ee9c14564370c435b67f6786978
Connor Brown <connorbrown1@usf.edu>
2025-07-17 00:04:54 -0400

    For this part I created a fetchProductsAsync() to use the async/await syntax also with the try/catch for error handleing. What this function does is it will fetch the product data asynchnonously and will pass it to the display function. Any errors that are there would be taken care of by the handleError().

--- cp_2.js
+++ cp_2.js
@@ -1,3 +1,4 @@
+//Using the function of fetch with .then() and .catch() to fetch product data from an API and log product names to the console.
 function fetchProductsThen() {
     fetch('https://www.course-api.com/javascript-store-products')
     .then(response => response.json())
@@ -9,6 +10,20 @@ console.log("Product names using .then():");
         console.error("Error in fetchProductsThen:", error.message);
     });
     }
+
+    //Function utilzing the async/await and the try/catch
+    async function fetchProductsAsync() {
+        try {
+            const response = await fetch('https://www.course-api.com/javascript-store-products');
+            const products = await response.json();
+            displayProducts(products);
+        } catch (error) {
+            handleError(error);
+        }
+    }
+
+    
+           
 
 
 
